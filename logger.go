232f86ec202b8efef3b97242425df6ae48debeb5
Rasarts <rasart.pro@gmail.com>
2018-05-31 07:31:02 +0300

    Init logger.

---
+++ logger.go
@@ -0,0 +1,59 @@
+package logger
+
+import (
+	"encoding/json"
+	"time"
+
+	"errors"
+
+	"github.com/hecatoncheir/Broker"
+)
+
+type LogData struct {
+	Message, Level string
+	Time           time.Time
+}
+
+type Writer interface {
+	Write(data LogData) error
+}
+
+type LogWriter struct {
+	APIVersion  string
+	LoggerTopic string
+	ServiceName string
+	bro         *broker.Broker
+}
+
+func New(apiVersion, serviceName, topicForWriteLog string, broker *broker.Broker) *LogWriter {
+	logger := LogWriter{LoggerTopic: topicForWriteLog, bro: broker, ServiceName: serviceName}
+	return &logger
+}
+
+var (
+	ErrLogDataWithoutTime = errors.New("log data without time")
+)
+
+func (logWriter *LogWriter) Write(data LogData) error {
+	if data.Time.IsZero() {
+		return ErrLogDataWithoutTime
+	}
+
+	item := map[string]interface{}{
+		"Service": data.Level, "Time": data.Time}
+
+	eventData, err := json.Marshal(item)
+	if err != nil {
+		return err
+	}
+
+	event := broker.EventData{
+		Message: data.Message, Data: string(eventData)}
+
+	err = logWriter.bro.WriteToTopic(logWriter.LoggerTopic, event)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ logger_test.go
@@ -0,0 +1,38 @@
+package logger
+
+import (
+	"testing"
+	"time"
+
+	"encoding/json"
+
+	"github.com/hecatoncheir/Broker"
+	"github.com/hecatoncheir/Configuration"
+)
+
+func TestLoggerCanWriteLogData(test *testing.T) {
+	conf := configuration.New()
+
+	bro := broker.New(conf.APIVersion, conf.ServiceName)
+	bro.Connect(conf.Development.Broker.Host, conf.Development.Broker.Port)
+
+	logWriter := New(conf.APIVersion, conf.ServiceName, conf.Development.LogunaTopic, bro)
+	logData := LogData{Message: "test message", Time: time.Now().UTC()}
+	go logWriter.Write(logData)
+
+	logunaTopic, err := bro.ListenTopic(conf.Development.LogunaTopic, conf.APIVersion)
+	if err != nil {
+		test.Fatal(err)
+	}
+
+	for event := range logunaTopic {
+		logEvent := LogData{}
+		json.Unmarshal(event, &logEvent)
+		if logEvent.Message != "test message" {
+			test.Fail()
+		}
+
+		break
+	}
+
+}
